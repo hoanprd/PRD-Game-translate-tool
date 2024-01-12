50245a392f954b302e16b99150dbe90371311b4d
hoanprd <99530243+hoanprd@users.noreply.github.com>
2024-01-12 09:29:02 +0700

    CF 12.01.2023 P1

--- PRD Game translate tool/frmTranslate.Designer.cs
+++ PRD Game translate tool/frmTranslate.Designer.cs
@@ -42,6 +42,9 @@ namespace PRD_Game_translate_tool
             this.càiĐặtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.chỉnhSửaNgônNgữToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnStopAuto = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picloading)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbxCapture)).BeginInit();
             this.menuStrip1.SuspendLayout();
@@ -50,10 +53,9 @@ namespace PRD_Game_translate_tool
             // btnCapture
             // 
             this.btnCapture.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btnCapture.Location = new System.Drawing.Point(771, 243);
-            this.btnCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCapture.Location = new System.Drawing.Point(897, 190);
             this.btnCapture.Name = "btnCapture";
-            this.btnCapture.Size = new System.Drawing.Size(100, 28);
+            this.btnCapture.Size = new System.Drawing.Size(75, 23);
             this.btnCapture.TabIndex = 0;
             this.btnCapture.Text = "Chụp hình";
             this.btnCapture.UseVisualStyleBackColor = true;
@@ -63,33 +65,30 @@ namespace PRD_Game_translate_tool
             // 
             this.txtCapture.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.txtCapture.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCapture.Location = new System.Drawing.Point(526, 87);
-            this.txtCapture.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCapture.Location = new System.Drawing.Point(605, 63);
             this.txtCapture.Multiline = true;
             this.txtCapture.Name = "txtCapture";
             this.txtCapture.ReadOnly = true;
-            this.txtCapture.Size = new System.Drawing.Size(345, 148);
+            this.txtCapture.Size = new System.Drawing.Size(367, 121);
             this.txtCapture.TabIndex = 1;
             // 
             // txtTranslate
             // 
             this.txtTranslate.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.txtTranslate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTranslate.Location = new System.Drawing.Point(526, 314);
-            this.txtTranslate.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTranslate.Location = new System.Drawing.Point(605, 247);
             this.txtTranslate.Multiline = true;
             this.txtTranslate.Name = "txtTranslate";
             this.txtTranslate.ReadOnly = true;
-            this.txtTranslate.Size = new System.Drawing.Size(345, 148);
+            this.txtTranslate.Size = new System.Drawing.Size(368, 121);
             this.txtTranslate.TabIndex = 3;
             // 
             // btnTranslate
             // 
             this.btnTranslate.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btnTranslate.Location = new System.Drawing.Point(771, 470);
-            this.btnTranslate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTranslate.Location = new System.Drawing.Point(897, 374);
             this.btnTranslate.Name = "btnTranslate";
-            this.btnTranslate.Size = new System.Drawing.Size(100, 28);
+            this.btnTranslate.Size = new System.Drawing.Size(75, 23);
             this.btnTranslate.TabIndex = 4;
             this.btnTranslate.Text = "Dịch";
             this.btnTranslate.UseVisualStyleBackColor = true;
@@ -100,7 +99,8 @@ namespace PRD_Game_translate_tool
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Location = new System.Drawing.Point(-5, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(151, 25);
             this.label1.TabIndex = 8;
@@ -111,7 +111,8 @@ namespace PRD_Game_translate_tool
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(521, 58);
+            this.label2.Location = new System.Drawing.Point(600, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(210, 25);
             this.label2.TabIndex = 9;
@@ -122,7 +123,8 @@ namespace PRD_Game_translate_tool
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(525, 285);
+            this.label3.Location = new System.Drawing.Point(604, 219);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(150, 25);
             this.label3.TabIndex = 10;
@@ -132,10 +134,10 @@ namespace PRD_Game_translate_tool
             // 
             this.picloading.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.picloading.Image = global::PRD_Game_translate_tool.Properties.Resources.icon_chara_face_0001;
-            this.picloading.Location = new System.Drawing.Point(663, 119);
-            this.picloading.Margin = new System.Windows.Forms.Padding(5);
+            this.picloading.Location = new System.Drawing.Point(766, 88);
+            this.picloading.Margin = new System.Windows.Forms.Padding(4);
             this.picloading.Name = "picloading";
-            this.picloading.Size = new System.Drawing.Size(92, 87);
+            this.picloading.Size = new System.Drawing.Size(69, 71);
             this.picloading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picloading.TabIndex = 7;
             this.picloading.TabStop = false;
@@ -144,23 +146,24 @@ namespace PRD_Game_translate_tool
             // pbxCapture
             // 
             this.pbxCapture.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.pbxCapture.Location = new System.Drawing.Point(12, 86);
-            this.pbxCapture.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pbxCapture.Location = new System.Drawing.Point(0, 63);
+            this.pbxCapture.Margin = new System.Windows.Forms.Padding(2);
             this.pbxCapture.Name = "pbxCapture";
-            this.pbxCapture.Size = new System.Drawing.Size(507, 414);
+            this.pbxCapture.Size = new System.Drawing.Size(600, 200);
             this.pbxCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbxCapture.TabIndex = 2;
             this.pbxCapture.TabStop = false;
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.càiĐặtToolStripMenuItem,
             this.thoátToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(884, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
             this.menuStrip1.TabIndex = 11;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -186,11 +189,48 @@ namespace PRD_Game_translate_tool
             this.thoátToolStripMenuItem.Text = "Thoát";
             this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
             // 
+            // btnAuto
+            // 
+            this.btnAuto.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnAuto.Location = new System.Drawing.Point(12, 268);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(264, 76);
+            this.btnAuto.TabIndex = 12;
+            this.btnAuto.Text = "Tự động";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnStopAuto
+            // 
+            this.btnStopAuto.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnStopAuto.Location = new System.Drawing.Point(335, 268);
+            this.btnStopAuto.Name = "btnStopAuto";
+            this.btnStopAuto.Size = new System.Drawing.Size(264, 76);
+            this.btnStopAuto.TabIndex = 13;
+            this.btnStopAuto.Text = "Dừng tự động";
+            this.btnStopAuto.UseVisualStyleBackColor = true;
+            this.btnStopAuto.Click += new System.EventHandler(this.btnStopAuto_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 347);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "00:00:00";
+            // 
             // frmTranslate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.ClientSize = new System.Drawing.Size(984, 441);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnStopAuto);
+            this.Controls.Add(this.btnAuto);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -201,7 +241,6 @@ namespace PRD_Game_translate_tool
             this.Controls.Add(this.pbxCapture);
             this.Controls.Add(this.txtCapture);
             this.Controls.Add(this.btnCapture);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmTranslate";
             this.Text = "frmTranslate";
             this.Load += new System.EventHandler(this.frmTranslate_Load);
@@ -229,5 +268,8 @@ namespace PRD_Game_translate_tool
         private System.Windows.Forms.ToolStripMenuItem càiĐặtToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem chỉnhSửaNgônNgữToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnStopAuto;
+        private System.Windows.Forms.Label label4;
     }
 }
--- PRD Game translate tool/frmTranslate.cs
+++ PRD Game translate tool/frmTranslate.cs
@@ -18,6 +18,7 @@ using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 using Tesseract;
+using Timer = System.Windows.Forms.Timer;
 
 namespace PRD_Game_translate_tool
 {
@@ -27,6 +28,7 @@ namespace PRD_Game_translate_tool
         static string[] langarrmini = { "vie", "ara", "bul", "chs", "cht", "hrv", "cze", "dan", "dut", "eng", "fin", "fre", "ger", "gre", "hun", "kor", "ita", "jpn", "pol", "por", "rus", "slv", "spa", "swe", "tur" };
         //string pathTemp = null;
         string langOCR, langInput, langOutput;
+        bool auto;
 
         public frmTranslate()
         {
@@ -59,10 +61,20 @@ namespace PRD_Game_translate_tool
             var width = bitmapSource.PixelWidth;
             var height = bitmapSource.PixelHeight;
             var stride = width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
-            Console.WriteLine(width + "," + height + "," + stride);
             var memoryBlockPointer = Marshal.AllocHGlobal(height * stride);
             bitmapSource.CopyPixels(new Int32Rect(0, 0, width, height), memoryBlockPointer, height * stride, stride);
             var bitmap = new Bitmap(width, height, stride, PixelFormat.Format32bppArgb, memoryBlockPointer);
+
+            return bitmap;
+        }
+
+        public static Bitmap ConvertToBitmapAuto(BitmapSource bitmapSource, System.Windows.Rect rc)
+        {
+            var stride = rc.Width * ((bitmapSource.Format.BitsPerPixel + 7) / 8);
+            var memoryBlockPointer = Marshal.AllocHGlobal((int)(rc.Height * stride));
+            bitmapSource.CopyPixels(new Int32Rect(0, 0, (int)rc.Width, (int)rc.Height), (IntPtr)memoryBlockPointer, (int)(rc.Width * stride), (int)stride);
+            var bitmap = new Bitmap((int)rc.Width, (int)rc.Height, (int)stride, PixelFormat.Format32bppArgb, memoryBlockPointer);
+
             return bitmap;
         }
 
@@ -79,21 +91,47 @@ namespace PRD_Game_translate_tool
 
         private void btnCapture_Click(object sender, EventArgs e)
         {
-            /*this.Hide();
-            Thread.Sleep(200);
-            frmct.ShowDialog();
-            Image tempImage = System.Windows.Forms.Clipboard.GetImage();
-            pbxCapture.Image = tempImage;
-            this.Show();
-
-            if (pbxCapture.Image != null)
+            if (!auto)
             {
-                pbxCapture.Image.Save(pathTemp + "/abc.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-            }*/
+                this.WindowState = FormWindowState.Minimized;
+                Thread.Sleep(500);
+                pbxCapture.Image = (Image)ConvertToBitmap(Screenshot.CaptureRegion());
+                this.WindowState = FormWindowState.Normal;
 
-            this.WindowState = FormWindowState.Minimized;
+                txtCapture.Text = "";
+                string result = "";
+                Task.Factory.StartNew(() => {
+                    picloading.BeginInvoke(new Action(() =>
+                    {
+                        picloading.Visible = true;
+                    }));
+
+                    result = OCR((Bitmap)pbxCapture.Image);
+                    txtCapture.BeginInvoke(new Action(() => {
+
+                        txtCapture.Text = result;
+
+                    }));
+                    picloading.BeginInvoke(new Action(() =>
+                    {
+                        picloading.Visible = false;
+                    }));
+
+                });
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Đang ở chế độ auto, vui lòng tắt!");
+            }
+            /*this.WindowState = FormWindowState.Minimized;
             Thread.Sleep(500);
-            pbxCapture.Image = (Image)ConvertToBitmap(Screenshot.CaptureRegion());
+            //pbxCapture.Image = (Image)ConvertToBitmap(Screenshot.CaptureRegion());
+            System.Windows.Rect myRect = new System.Windows.Rect();
+            //myRect.Location = new System.Windows.Point(510, 800);
+            //myRect.Size = new System.Windows.Size(1000, 180);
+            myRect.Location = new System.Windows.Point(510, 800);
+            myRect.Size = new System.Windows.Size(950, 160);
+            pbxCapture.Image = (Image)ConvertToBitmap(Screenshot.CaptureRegion(myRect), myRect);
             this.WindowState = FormWindowState.Normal;
 
             txtCapture.Text = "";
@@ -115,7 +153,7 @@ namespace PRD_Game_translate_tool
                     picloading.Visible = false;
                 }));
 
-            });
+            });*/
         }
 
         private void chỉnhSửaNgônNgữToolStripMenuItem_Click(object sender, EventArgs e)
@@ -126,7 +164,97 @@ namespace PRD_Game_translate_tool
 
         private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        Timer tmr = new Timer();
+        static int autoRectLocationX, autoRectLocationY, autoRectSizeX, autoRectSizeY;
+
+        public static void TakeAutoTemplate(BitmapSource bitmapSource)
+        {
+            int mouseX = MousePosition.X;
+            int mouseY = MousePosition.Y;
+            autoRectLocationX = mouseX - bitmapSource.PixelWidth; ;
+            autoRectLocationY = mouseY - bitmapSource.PixelHeight;
+            Console.WriteLine(autoRectLocationX + " " + autoRectLocationY);
+            autoRectSizeX = bitmapSource.PixelWidth;
+            autoRectSizeY = bitmapSource.PixelHeight;
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            if (!auto)
+            {
+                auto = true;
+                this.WindowState = FormWindowState.Minimized;
+                TakeAutoTemplate(Screenshot.CaptureRegion());
+                this.WindowState = FormWindowState.Normal;
+            }
+
+            if (auto)
+            {
+                tmr.Interval = 1000;
+                tmr.Tick += Tmr_Tick;
+                tmr.Start();
+            }
+            else
+            {
+                tmr.Stop();
+            }
+        }
+
+        private void Tmr_Tick(object sender, EventArgs e)
+        {
+            //this.WindowState = FormWindowState.Minimized;
+            //Thread.Sleep(500);
+            //System.Windows.Rect myRect = new System.Windows.Rect();
+            //myRect.Location = new System.Windows.Point(510, 800);
+            //myRect.Size = new System.Windows.Size(950, 160);
+            //pbxCapture.Image = (Image)ConvertToBitmap(Screenshot.CaptureRegion(myRect), myRect);
+
+            try
+            {
+                System.Windows.Rect myRect = new System.Windows.Rect();
+                myRect.Location = new System.Windows.Point(autoRectLocationX, autoRectLocationY);
+                myRect.Size = new System.Windows.Size(autoRectSizeX, autoRectSizeY);
+                pbxCapture.Image = (Image)ConvertToBitmapAuto(Screenshot.CaptureRegion(myRect), myRect);
+
+                txtCapture.Text = "";
+                string result = "";
+                Task.Factory.StartNew(() => {
+                    picloading.BeginInvoke(new Action(() =>
+                    {
+                        //picloading.Visible = true;
+                    }));
+
+                    result = OCR((Bitmap)pbxCapture.Image);
+                    txtCapture.BeginInvoke(new Action(() => {
+
+                        if (!String.IsNullOrEmpty(result) && !String.IsNullOrWhiteSpace(result))
+                        {
+                            txtCapture.Text = result;
+                            txtTranslate.Text = TranslateText(txtCapture.Text);
+                        }
+                    }));
+                    picloading.BeginInvoke(new Action(() =>
+                    {
+                        //picloading.Visible = false;
+                    }));
+
+                });
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnStopAuto_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+            auto = false;
+            txtCapture.Refresh();
+            txtTranslate.Refresh();
         }
 
         private void frmTranslate_Load(object sender, EventArgs e)
@@ -137,7 +265,12 @@ namespace PRD_Game_translate_tool
 
         private void btnTranslate_Click(object sender, EventArgs e)
         {
-            txtTranslate.Text = TranslateText(txtCapture.Text);
+            if (!auto)
+                txtTranslate.Text = TranslateText(txtCapture.Text);
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("Đang ở chế độ auto, vui lòng tắt!");
+            }
         }
     }
 }
